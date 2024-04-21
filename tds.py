fa5087ededa34b7c0f91535696f1ffbe86d02747
Ayushi Patel <121036083+A0129s@users.noreply.github.com>
2024-04-21 21:18:19 +0530

    Update tds.py

--- tds.py
+++ tds.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-def largest(a,b,c):
-  l=[]
-  l.append(a)
-  l.append(b)
-  l.append(c)
-  return max(l)
 
 st.title("Largest in three numbers")
 
@@ -13,7 +7,12 @@ x = st.number_input("Enter first number:", step = 0.0001, value = 0.0000, format
 y = st.number_input("Enter second number:", step = 0.0001, value = 0.0000, format = "%5.4f")
 z = st.number_input("Enter third number:", step = 0.0001, value = 0.0000, format = "%5.4f")
 
+
+greatest = max(n1, n2, n3)
+greatest = round(greatest, 4)
+
 st.text("The largest among these three numbers:")
-st.write(largest(x,y,z))
+if st.button("Click me!"):
+    st.balloons()
 
          
